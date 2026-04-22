47a7492b2cdc47fcf96f3268d3a210961534dc33
1Michael23 <>
2026-04-22 19:57:51 +1000

    aerospace keybind change

--- modules/home/darwin/aerospace.nix
+++ modules/home/darwin/aerospace.nix
@@ -7,6 +7,14 @@
 
 {
   xdg.configFile."aerospace/aerospace.toml".text = ''
+    start-at-login = true
+
+    enable-normalization-flatten-containers = true
+    enable-normalization-opposite-orientation-for-nested-containers = true
+    accordion-padding = 30
+    default-root-container-layout = 'tiles'
+    default-root-container-orientation = 'auto'
+
     [gaps]
     inner.horizontal = 5
     inner.vertical   = 5
@@ -15,48 +23,50 @@
     outer.top        = 5
     outer.bottom     = 5
 
+
     [mode.main.binding]
-    cmd-enter       = "exec-and-forget open -a Terminal"
-    cmd-space       = "exec-and-forget open -a Raycast"
-    cmd-q           = "close"
-    cmd-shift-c     = "reload-config"
-    cmd-shift-space = "layout floating tiling"
-    cmd-f           = "fullscreen"
+    alt-enter       = "exec-and-forget open -a Terminal"
+    alt-shift-q     = "close"
+    alt-shift-c     = "reload-config"
+    alt-shift-space = "layout floating tiling"
+    alt-shift-f     = "fullscreen"
 
-    cmd-left  = "focus left"
-    cmd-down  = "focus down"
-    cmd-up    = "focus up"
-    cmd-right = "focus right"
+    alt-left  = "focus left"
+    alt-down  = "focus down"
+    alt-up    = "focus up"
+    alt-right = "focus right"
 
-    cmd-shift-left  = "move left"
-    cmd-shift-down  = "move down"
-    cmd-shift-up    = "move up"
-    cmd-shift-right = "move right"
+    alt-shift-left  = "move left"
+    alt-shift-down  = "move down"
+    alt-shift-up    = "move up"
+    alt-shift-right = "move right"
 
-    cmd-shift-period = "move-workspace-to-monitor next"
-    cmd-shift-comma  = "move-workspace-to-monitor prev"
+    alt-shift-period = "move-workspace-to-monitor next"
+    alt-shift-comma  = "move-workspace-to-monitor prev"
 
-    cmd-1 = "workspace 1"
-    cmd-2 = "workspace 2"
-    cmd-3 = "workspace 3"
-    cmd-4 = "workspace 4"
-    cmd-5 = "workspace 5"
-    cmd-6 = "workspace 6"
-    cmd-7 = "workspace 7"
-    cmd-8 = "workspace 8"
-    cmd-9 = "workspace 9"
-    cmd-0 = "workspace 10"
+    alt-1 = "workspace 1"
+    alt-2 = "workspace 2"
+    alt-3 = "workspace 3"
+    alt-4 = "workspace 4"
+    alt-5 = "workspace 5"
+    alt-6 = "workspace 6"
+    alt-7 = "workspace 7"
+    alt-8 = "workspace 8"
+    alt-9 = "workspace 9"
+    alt-0 = "workspace 10"
 
-    cmd-shift-1 = "move-node-to-workspace 1"
-    cmd-shift-2 = "move-node-to-workspace 2"
-    cmd-shift-3 = "move-node-to-workspace 3"
-    cmd-shift-4 = "move-node-to-workspace 4"
-    cmd-shift-5 = "move-node-to-workspace 5"
-    cmd-shift-6 = "move-node-to-workspace 6"
-    cmd-shift-7 = "move-node-to-workspace 7"
-    cmd-shift-8 = "move-node-to-workspace 8"
-    cmd-shift-9 = "move-node-to-workspace 9"
-    cmd-shift-0 = "move-node-to-workspace 10"
+    alt-shift-1 = "move-node-to-workspace 1"
+    alt-shift-2 = "move-node-to-workspace 2"
+    alt-shift-3 = "move-node-to-workspace 3"
+    alt-shift-4 = "move-node-to-workspace 4"
+    alt-shift-5 = "move-node-to-workspace 5"
+    alt-shift-6 = "move-node-to-workspace 6"
+    alt-shift-7 = "move-node-to-workspace 7"
+    alt-shift-8 = "move-node-to-workspace 8"
+    alt-shift-9 = "move-node-to-workspace 9"
+    alt-shift-0 = "move-node-to-workspace 10"
+
+    alt-r = "mode resize"
 
     [mode.resize.binding]
     left   = "resize width -50"
