a20bcc315fe351920d446f121c157d19ddc13911
1Michael23 <33250781+1Michael23@users.noreply.github.com>
2026-04-23 14:16:07 +1000

    neovim plugins

--- modules/home/editors/neovim.nix
+++ modules/home/editors/neovim.nix
@@ -11,12 +11,76 @@
     defaultEditor = true;
     withRuby = false;
     withPython3 = false;
+
+    extraPackages = with pkgs; [
+      nil
+      nixd
+      nixfmt-rfc-style
+      rust-analyzer
+      pyright
+      ruff
+    ];
+
     initLua = ''
       vim.o.number = true
       vim.o.relativenumber = true
-      vim.o.termguicolors = true'';
+      vim.o.termguicolors = true
+      vim.o.signcolumn = "yes"
+      vim.o.cursorline = true
+      vim.o.scrolloff = 8
+      vim.o.sidescrolloff = 8
+      vim.o.wrap = false
+
+      vim.o.expandtab = true
+      vim.o.shiftwidth = 4
+      vim.o.tabstop = 4
+      vim.o.softtabstop = 4
+      vim.o.smartindent = true
+
+      vim.o.ignorecase = true
+      vim.o.smartcase = true
+      vim.o.incsearch = true
+      vim.o.hlsearch = false
+
+      vim.o.undofile = true
+      vim.o.updatetime = 250
+      vim.o.timeoutlen = 300
+      vim.o.clipboard = "unnamedplus"
+      vim.o.splitright = true
+      vim.o.splitbelow = true
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.nix",
+        callback = function() vim.lsp.buf.format() end,
+      })
+
+    '';
     plugins = with pkgs.vimPlugins; [
-      #TODO neovim plugins
+      catppuccin-nvim
+
+      telescope-nvim
+      plenary-nvim
+
+      (nvim-treesitter.withPlugins (p: [
+        p.nix
+        p.lua
+        p.rust
+        p.python
+        p.bash
+        p.markdown
+        p.yaml
+        p.toml
+        p.json
+      ]))
+
+      nvim-tree-lua
+      nvim-web-devicons
+
+      lualine-nvim
+
+      gitsigns-nvim
+      vim-fugitive
+
     ];
   };
 }
