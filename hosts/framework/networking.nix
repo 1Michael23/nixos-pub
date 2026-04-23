61cd21d1840a91202a38f9fc9d1cdf5e7474614d
1Michael23 <33250781+1Michael23@users.noreply.github.com>
2026-04-23 11:38:47 +1000

    networking tweaks

--- hosts/framework/networking.nix
+++ hosts/framework/networking.nix
@@ -8,22 +8,21 @@
 {
   networking = {
     hostName = "framework";
-    networkmanager.enable = true;
-    networkmanager.dns = "systemd-resolved";
-    networkmanager.wifi.powersave = true;
-    networkmanager.wifi.macAddress = "stable-ssid";
+    networkmanager = {
+      enable = true;
+      dns = "systemd-resolved";
+      wifi.powersave = true;
+      wifi.macAddress = "random";
+    };
     firewall = {
       enable = true;
-      checkReversePath = "loose"; # tailscale exit node patch
-      allowedTCPPorts = [
-        22000
-        8384
-        22
-      ]; # 22000 8384 syncthing 22 ssh
+      checkReversePath = "loose";
+      allowedTCPPorts = [ 22000 ];
       allowedUDPPorts = [
         22000
         21027
-      ]; # 21027 syncthing
+      ];
+      trustedInterfaces = [ "tailscale0" ];
     };
   };
 
@@ -31,24 +30,23 @@
     enable = true;
     dnssec = "false";
     fallbackDns = [
-      "10.30.0.7"
       "1.1.1.1"
     ];
   };
 
-  environment.etc."ssl/openssl.cnf".text = ''
-    openssl_conf = default_conf
+  # environment.etc."ssl/openssl.cnf".text = ''
+  #   openssl_conf = default_conf
 
-    [default_conf]
-    ssl_conf = ssl_sect
+  #   [default_conf]
+  #   ssl_conf = ssl_sect
 
-    [ssl_sect]
-    system_default = system_default_sect
+  #   [ssl_sect]
+  #   system_default = system_default_sect
 
-    [system_default_sect]
-    MinProtocol = TLSv1
-    CipherString = DEFAULT:@SECLEVEL=1
-  ''; # fix macquarie onenet
+  #   [system_default_sect]
+  #   MinProtocol = TLSv1
+  #   CipherString = DEFAULT:@SECLEVEL=1
+  # ''; # fix macquarie onenet
 
   programs.wireshark = {
     enable = true;
--- modules/system/networking/tailscale.nix
+++ modules/system/networking/tailscale.nix
@@ -3,8 +3,19 @@
   config = lib.mkIf pkgs.stdenv.isLinux {
     services.tailscale = {
       enable = true;
-      extraSetFlags = [ "--operator=michael" ];
+      openFirewall = true;
+      useRoutingFeatures = "both";
+      extraSetFlags = [
+        "--operator=michael"
+        "--ssh"
+      ];
+
     };
     networking.firewall.trustedInterfaces = [ "tailscale0" ];
+    # IP forwarding for exit-node / subnet-router functionality:
+    boot.kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
   };
 }
